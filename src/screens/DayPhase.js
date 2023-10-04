9a01ad1f19117fa655b13b7a7372adb7e2974f15
behlulsahin <80529961+voiicez@users.noreply.github.com>
2023-10-04 07:23:04 +0300

    Log

--- src/screens/DayPhase.js
+++ src/screens/DayPhase.js
@@ -4,6 +4,7 @@ import {useEffect} from 'react';
 import { useSelector } from 'react-redux';
 import { setNavigatedFromMarket } from './navigationSlice';
 import { useDispatch } from 'react-redux';
+import { clearNightEvents } from './gameSlice';
 
 // Inside DayPhaseScreen component
 const DayPhaseScreen = ({ route, navigation }) => {
@@ -13,26 +14,35 @@ const DayPhaseScreen = ({ route, navigation }) => {
     const { robberyOccurred } = route.params;
     const updatedPlayer=route.params.updatedPlayer;
     const navigatedFromMarket = useSelector(state => state.navigation.navigatedFromMarket);
+    const nightEvents = useSelector(state => state.game.nightEvents);
+    
     useEffect(() => {
+      
       dispatch(setNavigatedFromMarket(false));
+      return () => {
+        dispatch(clearNightEvents());
+      };
   }, [dispatch]);
-    console.log(navigatedFromMarket)
-    console.log('[DAYPHASE SCREEN] Roles in DayPhaseScreen:', roles);
-    console.log('[DAYPHASE SCREEN] Updated Player in DayPhaseScreen:', updatedPlayer);
+
 
   
   
-    return (
-      <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{ fontSize: 30, marginBottom: 20 }}>Day Phase</Text>
-        {robberyOccurred ? (
-          <Text>A robbery happened last night.</Text>
-        ) : (
-          <Text>Nothing unusual happened last night.</Text>
-        )}
-        <Button title="Ready" onPress={() => navigation.navigate('CountDown', { actions,roles,updatedPlayer })} />
-      </SafeAreaView>
-    );
+  return (
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 30, marginBottom: 20 }}>Day Phase</Text>
+      
+      {/* Conditionally render events or the "nothing happened" message */}
+      {nightEvents.length > 0 ? (
+        nightEvents.map((event, index) => (
+          <Text key={index}>{event}</Text>
+        ))
+      ) : (
+        <Text>Nothing happened last night.</Text>
+      )}
+
+      <Button title="Ready" onPress={() => navigation.navigate('CountDown', { actions, roles, updatedPlayer })} />
+    </SafeAreaView>
+  );
   };
   
 export default DayPhaseScreen;
