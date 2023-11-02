ed48f178ae65b5f2f7f67264d3583db5f85539b8
Behlül Şahin <voiicez@gmail.com>
2023-11-02 04:48:28 +0300

    VotingScreen

--- src/screens/VotingPhaseScreen.js
+++ src/screens/VotingPhaseScreen.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button,TouchableOpacity,Image,ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-
+import cardImage from '../assets/images/votingCard.png';
 const VotingPhaseScreen = ({ navigation, route }) => {
   const [votes, setVotes] = useState(route.params.roles.map(() => 0));
   const roles = route.params.roles;
-  const updatedPlayer=route.params.updatedPlayer;
+  const updatedPlayer = route.params.updatedPlayer;
+
   const castVote = (index) => {
     const newVotes = [...votes];
     newVotes[index]++;
@@ -16,20 +17,28 @@ const VotingPhaseScreen = ({ navigation, route }) => {
     const maxVotes = Math.max(...votes);
     const eliminatedIndex = votes.indexOf(maxVotes);
     const eliminatedPlayer = roles[eliminatedIndex];
-    // Navigate to the elimination screen with the eliminated player
-    navigation.navigate('Elimination', { eliminatedPlayer, roles: roles.filter((_, i) => i !== eliminatedIndex),updatedPlayer });
+    navigation.navigate('Elimination', { eliminatedPlayer, roles: roles.filter((_, i) => i !== eliminatedIndex), updatedPlayer });
   };
 
   return (
     <SafeAreaView style={{ flex: 1, padding: 20 }}>
-      <Text style={{ fontSize: 20, marginBottom: 10 }}>Voting Phase</Text>
-      {roles.map((player, index) => (
-        <View key={index} style={{ marginBottom: 10 }}>
-          <Text style={{ fontSize: 18 }}>{player.name}: {votes[index]} votes</Text>
-          <Button title="Vote" onPress={() => castVote(index)} />
+      <Text style={{ fontSize: 20, marginBottom: 10,textAlign:'center' }}>Elemek istediğiniz oyuncuyu oylayın.</Text>
+      <ScrollView>
+        <View style={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between' }}>
+          {roles.map((player, index) => (
+            <View key={index} style={{ width: '48%', marginBottom: 10, alignItems: 'center' }}>
+              <TouchableOpacity onPress={() => castVote(index)} style={{ position: 'relative' }}>
+                <Image source={cardImage} style={{ width: 150, height: 150 }} />
+                <View style={{ position: 'absolute', bottom: 7, left: 0, right: 0, flexDirection: 'row', justifyContent: 'center' }}>
+                  <Text style={{ fontSize: 18, color: 'white' }}>{player.name}</Text>
+                </View>
+              </TouchableOpacity>
+              <Text style={{ fontSize: 18, marginTop: 10 }}>{votes[index]}</Text>
+            </View>
+          ))}
         </View>
-      ))}
-      <Button title="End Voting" onPress={endVoting} />
+        <Button title="End Voting" onPress={endVoting} />
+      </ScrollView>
     </SafeAreaView>
   );
 };
